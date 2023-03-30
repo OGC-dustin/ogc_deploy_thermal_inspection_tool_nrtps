bae4b56aa7f0008eda0ae5f0e1eee74c2a5959fe
Dustin <dustin@ogc.engineering>
2023-03-30 17:36:35 -0500

    working gpio to control RGB LED

---
+++ app.h
@@ -0,0 +1,6 @@
+#ifndef APP_H
+#define APP_H
+
+#include "ogc_app_thermal_inspection_tool.h"
+
+#endif /* APP_H */
---
+++ deployment.h
@@ -0,0 +1,6 @@
+#ifndef DEPLOYMENT_H
+#define DEPLOYMENT_H
+
+#define OGC_LIB_OS_NRTPS__EVENT_ARRAY_SIZE ( 5U )
+
+#endif /* DEPLOYMENT_H */
